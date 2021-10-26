3fd44f3f922dae4cd26a4fe6a477692cdd7b097b
waellatif <waellatif88@gmail.com>
2021-10-26 22:55:45 +0200

    Shopping app

--- app/src/main/java/com/example/ecommerce/AdminAddNewProductActivity.java
+++ app/src/main/java/com/example/ecommerce/AdminAddNewProductActivity.java
@@ -3,9 +3,15 @@ package com.example.ecommerce;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class AdminAddNewProductActivity extends AppCompatActivity {
 
+    private Button addNewProductBtn;
+    private EditText inputProductName, inputProductDescription, inputProductPrice ;
+    private ImageView productImage;
     private String CategoryName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +19,14 @@ public class AdminAddNewProductActivity extends AppCompatActivity {
         setContentView(R.layout.activity_admin_add_new_product);
 
         CategoryName = getIntent().getStringExtra("category");
+
+        addNewProductBtn =findViewById(R.id.addProductBtn);
+        productImage = findViewById(R.id.selectProductImage);
+        inputProductName=findViewById(R.id.productName);
+        inputProductDescription=findViewById(R.id.productDescription);
+        inputProductPrice=findViewById(R.id.productPrice);
+
+
     }
 
 }
